b92ae52460f525d67da17dd5a5cf5b6ff1bf4b2a
Kevin Ho <keho98@gmail.com>
2015-02-24 23:48:01 -0800

    Initial Commit

---
+++ bnrfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bnrfeed
+//
+//  Created by Kevin Ho on 2/24/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bnrfeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bnrfeed
+//
+//  Created by Kevin Ho on 2/24/15.
+//  Copyright (c) 2015 kho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
